f926e7636369cae32937c7cc59674b6fdd53b2ea
Goutham J <54531058+goutham-j@users.noreply.github.com>
2021-08-29 20:31:06 +0530

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'Goutham', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'Goutham', 'Learn Spring Boot', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'Goutham', 'Learn Microservices', sysdate(), false);
